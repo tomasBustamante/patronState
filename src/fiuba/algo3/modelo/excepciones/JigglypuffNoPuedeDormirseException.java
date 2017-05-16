75f144a8dec2a2f38b1fdc25ba72b76392d04496
Tomás Bustamante <bustamantejt@gmail.com>
2017-05-16 14:53:46 -0300

    Updated exceptions

---
+++ src/fiuba/algo3/modelo/excepciones/JigglypuffNoPuedeDormirseException.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.modelo.excepciones;
+
+public class JigglypuffNoPuedeDormirseException extends RuntimeException {
+
+}
---
+++ src/fiuba/algo3/modelo/excepciones/PokemonEstaDormidoException.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.modelo.excepciones;
+
+public class PokemonEstaDormidoException extends RuntimeException {
+
+}
