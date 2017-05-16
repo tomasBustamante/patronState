7daa3505673fa5d23070e1b379b18e70666f3f80
Tomás Bustamante <bustamantejt@gmail.com>
2017-05-16 14:51:53 -0300

    First state

---
+++ src/fiuba/algo3/modelo/estados/EstadoPikachu.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.modelo.estados;
+
+import fiuba.algo3.modelo.pokemones.Pikachu;
+import fiuba.algo3.modelo.pokemones.Pokemon;
+
+public interface EstadoPikachu {
+
+    public abstract void impacTrueno(Pikachu pikachu, Pokemon otroAlgomon);
+
+}
